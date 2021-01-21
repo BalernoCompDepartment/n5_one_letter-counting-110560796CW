e636c7f00c138d77cc6f7023d2d1ff846f850c3c
110560796CW <110560796@ea.edin.sch.uk>
2021-01-21 09:39:17 +0000

    letters counter

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    
+
     // ask user for the words and store as message
+    String message = Keyboard.getText("what is your message?");
+    String output = (" ");
 
     // store the number of the letters as numberLetters
-    
+    int numbersLetters = message.length();
     // display "There are " + numberLetters + " in " + message
+    output = ("There are " + numbersLetters + " in " + message);
+
+    Screen.display(output, "message length");
 
   }
 }
